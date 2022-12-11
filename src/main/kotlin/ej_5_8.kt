a3b4af7abb443b0411d8dfbb64b07f5fabe85356
Alexgf22 <agarcia@iesrafaelalberti.es>
2022-12-11 16:55:30 +0100

    empezando clase tiempo

--- src/main/kotlin/ej_5_8.kt
+++ src/main/kotlin/ej_5_8.kt
@@ -1,25 +1,40 @@
-/*class Tiempo(
+class Tiempo(
     var hora: Int,
     var minuto: Int = 0,
     var segundo: Int = 0
 ) {
+
+    /*constructor(hora: Int,minuto: Int): this(hora,minuto) {
+        this.hora = hora
+        this.minuto = minuto
+    }
+
+    constructor(hora: Int): this(hora) {
+        this.hora = hora
+    }*/
+
+
 
     override fun toString(): String {
         return "$hora h $minuto m $segundo s"
     }
 
 
-}*/
+}
 
-/*
+
 fun main() {
-    print("Introduce la hora")
-    var la_hora = readLine?.toInt()
+    print("Introduce la hora: ")
+    var hora = readln().toInt()
 
-    print("Introduce los minutos")
-    var los_minutos = readLine?.toInt()
+    print("Introduce los minutos: ")
+    var minutos = readln().toInt()
 
-    print("Introduce los segundos")
-    var los_segundos = readLine?.toInt()
+    print("Introduce los segundos: ")
+    var segundos = readln().toInt()
 
-}*/
+    var tiempo1 = Tiempo(hora,minutos,segundos)
+
+    print(tiempo1.toString())
+
+}
