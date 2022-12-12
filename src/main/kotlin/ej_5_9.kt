405ede71fefcdb8b75f7b5e0df0f5e5e02b03264
Alexgf22 <agarcia@iesrafaelalberti.es>
2022-12-12 12:31:36 +0100

    cambios clase personas, movimientos de cuentas realizados, creada cuenta nueva, añadir saldo, enviar saldo

--- src/main/kotlin/ej_5_9.kt
+++ src/main/kotlin/ej_5_9.kt
@@ -1,23 +1,25 @@
-import java.util.spi.CurrencyNameProvider
-
 open class Cuenta(
     var numero_cuenta: String,
     var saldo: Int
 
 ) {
 
-    fun consultar_saldo(): Int {
+    fun consultarSaldo(): Int {
         return saldo
     }
 
-    fun recibir_abonos(cantidadRecibida: Int): String {
+    fun recibirAbonos(cantidadRecibida: Int): Int {
         saldo += cantidadRecibida
-        return "Has recibido: $cantidadRecibida € y ahora tu saldo es de $saldo"
+        return cantidadRecibida
     }
 
-    fun realizar_pagos(cantidadPagar: Int): String {
+    fun realizarPagos(cantidadPagar: Int): Int {
         saldo -= cantidadPagar
-        return "Has pagado: $cantidadPagar y ahora tu saldo es de $saldo"
+        return cantidadPagar
+    }
+
+    override fun toString(): String {
+        return "La cuenta: $numero_cuenta con: $saldo euros de saldo"
     }
 
 
@@ -32,25 +34,45 @@ class Persona(
 ): Cuenta(numero_cuenta="ES1034734343", saldo =100 ) {
 
     var contadorCuentas = 0
-    fun añadir_cuenta() {
+
+    fun añadirCuenta(numero_cuenta_nuevo: String, saldo_nuevo: Int): String {
+        var resultado = ""
+        if(contadorCuentas == 0) {
+            for (cuenta in cuentas) {
+                contadorCuentas += 1
+            }
+        }
+
         contadorCuentas += 1
         if(contadorCuentas <= 3) {
-            var cuentaNueva = Cuenta(numero_cuenta,saldo)
+            val cuentaNueva = Cuenta(numero_cuenta_nuevo,saldo_nuevo)
             cuentas.add(cuentaNueva)
         }
+        else {
+            resultado = "No se pueden añadir mas de 3 cuentas por persona"
+            return resultado
+        }
+
+        resultado = "La cuenta: $numero_cuenta_nuevo, con: $saldo_nuevo euros"
+        return resultado
 
     }
 
-    fun comprobar_moroso(): String {
+    fun comprobarMoroso(): String {
+        var resultado = ""
         for(cuenta in cuentas)  {
             if (cuenta.saldo < 0) {
-                return "La persona es morosa"
+                resultado = "La persona es morosa"
+            }
+            else {
+                resultado = "La persona no es morosa"
             }
         }
+        return resultado
     }
 
     override fun toString(): String {
-        return "La persona con DNI $dni"
+        return "La persona con DNI $dni, tiene las siguientes cuentas bancarias $cuentas"
     }
 
 
@@ -58,7 +80,20 @@ class Persona(
 
 
 fun main() {
+    val cuenta1 = Cuenta("ES10295625",0)
+    val cuenta2 = Cuenta("ES10347483",700)
+    val persona1 = Persona("53698124E", mutableListOf(cuenta1,cuenta2))
+
+    cuenta1.recibirAbonos(1100)
+    cuenta2.realizarPagos(750)
 
 
+    cuenta2.recibirAbonos(cuenta1.realizarPagos(300))
+    println("El saldo de la cuenta1 es ahora de: ${cuenta1.consultarSaldo()}")
+    println("El saldo de la cuenta2 es ahora de: ${cuenta2.consultarSaldo()}")
+    println(persona1.comprobarMoroso())
 
+    persona1.añadirCuenta("ES108582842",200)
+    persona1.añadirCuenta("ES105478854",400)
+    println(persona1.toString())
 }
